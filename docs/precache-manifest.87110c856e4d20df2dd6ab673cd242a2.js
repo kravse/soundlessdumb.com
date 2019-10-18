766856c5b7aed22f852ec2db6d2db108c6fadf96
jared <jared@tunnelbear.com>
2019-10-18 17:48:08 -0400

    docs

---
+++ docs/precache-manifest.87110c856e4d20df2dd6ab673cd242a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13bb732b49153faccd6fb6bdaf11cd2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff2704b0738ac26aa902",
+    "url": "/static/css/main.fd6385c9.chunk.css"
+  },
+  {
+    "revision": "0ffa0673339ddef84b97",
+    "url": "/static/js/2.12e0707f.chunk.js"
+  },
+  {
+    "revision": "ff2704b0738ac26aa902",
+    "url": "/static/js/main.5db04cd0.chunk.js"
+  },
+  {
+    "revision": "ba2388fa5af77e5b2225",
+    "url": "/static/js/runtime-main.06d4958e.js"
+  },
+  {
+    "revision": "35d91ed8b25a5b2da86d05ec285d4c42",
+    "url": "/static/media/smart-books.35d91ed8.jpg"
+  }
+]);
