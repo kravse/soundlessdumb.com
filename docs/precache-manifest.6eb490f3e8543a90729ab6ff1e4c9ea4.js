eb00b5a1dd2a6118487c8a7b370707a520e6eaa9
jared <jared@tunnelbear.com>
2019-10-26 23:53:57 -0400

    some mobile styles

---
+++ docs/precache-manifest.6eb490f3e8543a90729ab6ff1e4c9ea4.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703d8629a2c5070e0407553fa31aa6d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3dc6cb7a9cfe78b2fc0",
+    "url": "/static/css/main.f1bd7b78.chunk.css"
+  },
+  {
+    "revision": "d466776f88437498097f",
+    "url": "/static/js/0.362365f7.chunk.js"
+  },
+  {
+    "revision": "c883ccfd23f734934d14",
+    "url": "/static/js/1.4b0ff467.chunk.js"
+  },
+  {
+    "revision": "82c1860a3871623f2e11",
+    "url": "/static/js/10.8232cdf9.chunk.js"
+  },
+  {
+    "revision": "4c032748de3110e26775",
+    "url": "/static/js/11.40d2f00a.chunk.js"
+  },
+  {
+    "revision": "cdfb80e622c063391c89",
+    "url": "/static/js/12.5d7309a8.chunk.js"
+  },
+  {
+    "revision": "87d295a5ee4bf813b42e",
+    "url": "/static/js/13.67b79122.chunk.js"
+  },
+  {
+    "revision": "ecf0413d3aa313e67212",
+    "url": "/static/js/14.ffdf6d68.chunk.js"
+  },
+  {
+    "revision": "5b6846f105797dcd5614",
+    "url": "/static/js/15.49b000a6.chunk.js"
+  },
+  {
+    "revision": "16a4987c0eb5fccb3f59",
+    "url": "/static/js/16.eb16f671.chunk.js"
+  },
+  {
+    "revision": "c03455c834db8a9b2c7a",
+    "url": "/static/js/17.c0b084af.chunk.js"
+  },
+  {
+    "revision": "a432e2caeb3c36144ebb",
+    "url": "/static/js/18.1ee3ed2b.chunk.js"
+  },
+  {
+    "revision": "79320ba1e216112de3dd",
+    "url": "/static/js/19.fa4a41f8.chunk.js"
+  },
+  {
+    "revision": "1eb7773786c03b8e68b4",
+    "url": "/static/js/2.04451c62.chunk.js"
+  },
+  {
+    "revision": "5d52936d26687865c147",
+    "url": "/static/js/20.a22d5028.chunk.js"
+  },
+  {
+    "revision": "55ce54fa93e9fe8f1591",
+    "url": "/static/js/21.c1319366.chunk.js"
+  },
+  {
+    "revision": "b2eaade242b8e989633b",
+    "url": "/static/js/22.e7a41ec5.chunk.js"
+  },
+  {
+    "revision": "c5c9e8103d48f89da9bc",
+    "url": "/static/js/23.ed430e14.chunk.js"
+  },
+  {
+    "revision": "67324ce3568068926ac4",
+    "url": "/static/js/24.a2192340.chunk.js"
+  },
+  {
+    "revision": "02d586fb8a8ed7438afd",
+    "url": "/static/js/25.00cb93af.chunk.js"
+  },
+  {
+    "revision": "5c12de5c009d122fc851",
+    "url": "/static/js/28.208b2e93.chunk.js"
+  },
+  {
+    "revision": "a7c6aa9ae42795e07afc",
+    "url": "/static/js/3.1e509144.chunk.js"
+  },
+  {
+    "revision": "7644965b1c375555f911",
+    "url": "/static/js/4.cf206c72.chunk.js"
+  },
+  {
+    "revision": "0fe3b3a26a0bd58814df",
+    "url": "/static/js/5.28c3f054.chunk.js"
+  },
+  {
+    "revision": "52fce59cc57ac03739e6",
+    "url": "/static/js/6.fe3969c5.chunk.js"
+  },
+  {
+    "revision": "0b332d495ef46cd6b25a",
+    "url": "/static/js/7.b8928752.chunk.js"
+  },
+  {
+    "revision": "5cbc49306937bee4daa3",
+    "url": "/static/js/8.ca7714c7.chunk.js"
+  },
+  {
+    "revision": "59f5d9a115a94f35e173",
+    "url": "/static/js/9.b3967f70.chunk.js"
+  },
+  {
+    "revision": "d3dc6cb7a9cfe78b2fc0",
+    "url": "/static/js/main.7585295f.chunk.js"
+  },
+  {
+    "revision": "829348e5a8b933b391db",
+    "url": "/static/js/runtime-main.e194560e.js"
+  },
+  {
+    "revision": "35d91ed8b25a5b2da86d05ec285d4c42",
+    "url": "/static/media/smart-books.35d91ed8.jpg"
+  },
+  {
+    "revision": "5fe0e55f8e19bc4cc3201876dce7b224",
+    "url": "/static/media/spinner.5fe0e55f.gif"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sound-less-dumb/precache-manifest.effb4b603949032bbc2ce0de47bff4e9.js"
+  "/precache-manifest.6eb490f3e8543a90729ab6ff1e4c9ea4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sound-less-dumb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
